5a7efe850bd0dcdc5b4e0606e3c08f625fe9165a
Lion Dragon <mrrobot2015020309@gmail.com>
2026-05-05 15:26:20 +0300

    Revert "Pattern of Operation (static method WaitingForAnswer(), and generic method Solving(string answer (from WaitingFromAnswer)"
    This reverts commit 4811adc2b95fda8f63a01fbb1182aa68d0052b1e.

--- SmartCalculator/SCalc.cs
+++ SmartCalculator/SCalc.cs
@@ -4,24 +4,12 @@ using System.Text;
 
 namespace SmartCalculator
 {
-    public class SCalc<T>
+    public class SCalc
     {
        
         static SCalc()
         {
             Console.WriteLine("Welcome! What operations do you want me to complete?");
-            WaitingForAnswer();
-           
-        }
-
-        private static void WaitingForAnswer()
-        {
-          string answerOfUser=Console.ReadLine();
-          Solving(answerOfUser);
-        }
-
-        private T Solving(string answer)
-        {
 
         }
 
