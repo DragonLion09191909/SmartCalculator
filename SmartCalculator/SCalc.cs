4811adc2b95fda8f63a01fbb1182aa68d0052b1e
Lion Dragon <mrrobot2015020309@gmail.com>
2026-05-05 14:07:02 +0300

    Pattern of Operation (static method WaitingForAnswer(), and generic method Solving(string answer (from WaitingFromAnswer)

--- SmartCalculator/SCalc.cs
+++ SmartCalculator/SCalc.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace SmartCalculator
 {
-    public class SCalc
+    public class SCalc<T>
     {
        
         static SCalc()
         {
             Console.WriteLine("Welcome! What operations do you want me to complete?");
+            WaitingForAnswer();
+           
+        }
+
+        private static void WaitingForAnswer()
+        {
+          string answerOfUser=Console.ReadLine();
+          Solving(answerOfUser);
+        }
+
+        private T Solving(string answer)
+        {
 
         }
 
